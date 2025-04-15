c0d28fe905061146dfe2f468b02fa0d7b6a170a2
CanerTanriverdi <canertanriverdiis@gmail.com>
2025-04-15 08:09:06 +0300

    users chat done

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,9 +1,23 @@
 import React, { useState, useRef, useEffect } from "react";
 import EmojiPicker from "emoji-picker-react";
+import { arrayUnion, doc, getDoc, onSnapshot, updateDoc } from "firebase/firestore";
+import { db } from "../../lib/firebase";
+import { useChatStore } from "../../lib/chatStore";
+import { useUserStore } from "../../lib/userStore";
+import upload from "../../lib/upload";
 
 export default function Chat() {
+  const [chat, setChat] = useState();
   const [open, setOpen] = useState(false);
   const [text, setText] = useState("");
+  const [img, setImg] = useState({
+    file: null,
+    url: "",
+  });
+
+  const { currentUser } = useUserStore();
+  const { chatId, user } = useChatStore();
+
   const inputRef = useRef();
   const endRef = useRef(null);
 
@@ -11,166 +25,180 @@ export default function Chat() {
     endRef.current?.scrollIntoView({ behavior: "smooth" });
   });
 
+  useEffect(() => {
+    const unSub = onSnapshot(doc(db, "chats", chatId), (res) => {
+      setChat(res.data());
+    });
+
+    return () => {
+      unSub();
+    };
+  }, [chatId]);
+
   const handleEmoji = (emojiData) => {
     setText((prev) => prev + emojiData.emoji);
     setOpen(false);
     inputRef.current?.focus();
   };
 
+  const handleImg = (e) => {
+    if (e.target.files[0]) {
+      setImg({
+        file: e.target.files[0],
+        url: URL.createObjectURL(e.target.files[0]),
+      });
+    }
+  };
+
+  const handleSend = async () => {
+    if (text === "") return;
+
+    let imgUrl = null;
+
+    try {
+      if (img.file) {
+        imgUrl = await upload(img.file);
+      }
+
+      await updateDoc(doc(db, "chats", chatId), {
+        messages: arrayUnion({
+          senderId: currentUser.id,
+          text,
+          createdAt: new Date(),
+          ...(imgUrl && { img: imgUrl }),
+        }),
+      });
+
+      const userIDs = [currentUser.id, user.id];
+
+      userIDs.forEach(async (id) => {
+        const userChatsRef = doc(db, "userchats", id);
+        const userChatsSnapshot = await getDoc(userChatsRef);
+
+        if (userChatsSnapshot.exists()) {
+          const userChatsData = userChatsSnapshot.data();
+
+          const chatIndex = userChatsData.chats.findIndex((c) => c.chatId === chatId);
+
+          userChatsData.chats[chatIndex].lastMessage = text;
+          userChatsData.chats[chatIndex].isSeen = id === currentUser.id ? true : false;
+          userChatsData.chats[chatIndex].updatedAt = Date.now();
+
+          await updateDoc(userChatsRef, {
+            chats: userChatsData.chats,
+          });
+        }
+      });
+    } catch (err) {
+      console.log(err);
+    }
+
+    setImg({
+      file: null,
+      url: "",
+    });
+
+    setText("");
+  };
+
   return (
     <div className="flex flex-col flex-[2] border-l border-r border-l-[#dddddd35] border-r-[#dddddd35] h-full">
       <div className="top flex p-3 items-center justify-between border-b border-b-[#dddddd35]">
-        <div className="user flex items-center gap-3">
+        <div className="flex items-center gap-3 user">
           <img
             className="w-12 h-12 sm:w-[60px] sm:h-[60px] rounded-full object-cover"
             src="./avatar.png"
             alt="User avatar"
           />
-          <div className="texts flex flex-col gap-0">
+          <div className="flex flex-col gap-0 texts">
             <span className="text-[18px] font-bold">caner tanriverdi</span>
             <p className="text-[15px] font-extralight text-[#a5a5a5]">Lorem ipsum dolor sit amet.</p>
           </div>
         </div>
-        <div className="icons flex gap-4">
+        <div className="flex gap-4 icons">
           <img
-            className="w-4 h-4 sm:w-5 sm:h-5 cursor-pointer"
+            className="w-4 h-4 cursor-pointer sm:w-5 sm:h-5"
             src="./phone.png"
             alt="Phone"
           />
           <img
-            className="w-4 h-4 sm:w-5 sm:h-5 cursor-pointer"
+            className="w-4 h-4 cursor-pointer sm:w-5 sm:h-5"
             src="./video.png"
             alt="Video"
           />
           <img
-            className="w-4 h-4 sm:w-5 sm:h-5 cursor-pointer"
+            className="w-4 h-4 cursor-pointer sm:w-5 sm:h-5"
             src="./info.png"
             alt="Info"
           />
         </div>
       </div>
 
-      <div className="center flex flex-col p-5 gap-5 flex-1 overflow-auto">
-        <div className="message OWN flex max-w-[80%] self-end">
-          <div className="texts flex flex-1 flex-col gap-1">
-            <p className="bg-[#5183fe] rounded-lg text-sm p-2">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi corporis cumque deleniti quaerat obcaecati
-              quasi assumenda a similique accusantium dolorem doloremque, qui totam eveniet quisquam reprehenderit ad
-              earum eaque libero!
-            </p>
-            <span className="text-xs">1 min ago</span>
-          </div>
-        </div>
+      <div className="flex flex-col flex-1 gap-5 p-5 overflow-auto center">
+        {chat?.messages?.map((message) => {
+          // Bu mesaj bana mı ait?
+          const isOwner = message.senderId === currentUser.id;
 
-        <div className="message YOU flex gap-2 max-w-[80%]">
-          <img
-            className="w-6 h-6 sm:w-[30px] sm:h-[30px] rounded-full object-cover"
-            src="./avatar.png"
-            alt="User"
-          />
-          <div className="texts flex flex-1 flex-col gap-1">
-            <p className="text-sm p-2 rounded-lg bg-[rgba(17,25,40,0.3)]">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi corporis cumque deleniti quaerat obcaecati
-              quasi assumenda a similique accusantium dolorem doloremque, qui totam eveniet quisquam reprehenderit ad
-              earum eaque libero!
-            </p>
-            <span className="text-xs">1 min ago</span>
-          </div>
-        </div>
+          return (
+            <div
+              key={message.createdAt}
+              className={`message flex gap-2 max-w-[80%] ${isOwner ? "ml-auto flex-row-reverse" : ""}`}
+            >
+              {message.img && (
+                <img
+                  className="w-6 h-6 sm:w-[30px] sm:h-[30px] rounded-full object-cover"
+                  src="./avatar.png"
+                  alt="User"
+                />
+              )}
 
-        <div className="message OWN flex max-w-[80%] self-end">
-          <div className="texts flex flex-1 flex-col gap-1">
-            <p className="bg-[#5183fe] rounded-lg text-sm p-2">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi corporis cumque deleniti quaerat obcaecati
-              quasi assumenda a similique accusantium dolorem doloremque, qui totam eveniet quisquam reprehenderit ad
-              earum eaque libero!
-            </p>
-            <span className="text-xs">1 min ago</span>
-          </div>
-        </div>
+              <div className="flex flex-col flex-1 gap-1 max-w-[%80] texts">
+                {message.img && (
+                  <img
+                    src={message.img}
+                    alt="Shared media"
+                  />
+                )}
+                <p
+                  className={`text-sm p-2 rounded-lg ${
+                    isOwner ? "bg-[#5183fe] text-white" : "bg-[rgba(52,53,65,0.6)] text-white"
+                  }`}
+                >
+                  {message.text}
+                </p>
+                {/* <span>{message}</span> */}
+              </div>
+            </div>
+          );
+        })}
 
-        <div className="message YOU flex gap-2 max-w-[80%]">
-          <img
-            className="w-6 h-6 sm:w-[30px] sm:h-[30px] rounded-full object-cover"
-            src="./avatar.png"
-            alt="User"
-          />
-          <div className="texts flex flex-1 flex-col gap-1">
-            <p className="text-sm p-2 rounded-lg bg-[rgba(17,25,40,0.3)]">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi corporis cumque deleniti quaerat obcaecati
-              quasi assumenda a similique accusantium dolorem doloremque, qui totam eveniet quisquam reprehenderit ad
-              earum eaque libero!
-            </p>
-            <span className="text-xs">1 min ago</span>
+        {img.url && (
+          <div className="message own">
+            <div className="text">
+              <img
+                src={img.url}
+                alt=""
+              />
+            </div>
           </div>
-        </div>
-
-        <div className="message OWN flex max-w-[80%] self-end">
-          <div className="texts flex flex-1 flex-col gap-1">
-            <p className="bg-[#5183fe] rounded-lg text-sm p-2">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi corporis cumque deleniti quaerat obcaecati
-              quasi assumenda a similique accusantium dolorem doloremque, qui totam eveniet quisquam reprehenderit ad
-              earum eaque libero!
-            </p>
-            <span className="text-xs">1 min ago</span>
-          </div>
-        </div>
-
-        <div className="message YOU flex gap-2 max-w-[80%]">
-          <img
-            className="w-6 h-6 sm:w-[30px] sm:h-[30px] rounded-full object-cover"
-            src="./avatar.png"
-            alt="User"
-          />
-          <div className="texts flex flex-1 flex-col gap-1">
-            <p className="text-sm p-2 rounded-lg bg-[rgba(17,25,40,0.3)]">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi corporis cumque deleniti quaerat obcaecati
-              quasi assumenda a similique accusantium dolorem doloremque, qui totam eveniet quisquam reprehenderit ad
-              earum eaque libero!
-            </p>
-            <span className="text-xs">1 min ago</span>
-          </div>
-        </div>
-
-        <div className="message OWN flex max-w-[80%] self-end">
-          <div className="texts flex flex-1 flex-col gap-1">
-            <p className="bg-[#5183fe] rounded-lg text-sm p-2">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi corporis cumque deleniti quaerat obcaecati
-              quasi assumenda a similique accusantium dolorem doloremque, qui totam eveniet quisquam reprehenderit ad
-              earum eaque libero!
-            </p>
-            <span className="text-xs">1 min ago</span>
-          </div>
-        </div>
-
-        <div className="message YOU flex gap-2 max-w-[80%]">
-          <img
-            className="w-6 h-6 sm:w-[30px] sm:h-[30px] rounded-full object-cover"
-            src="./avatar.png"
-            alt="User"
-          />
-          <div className="texts flex flex-1 flex-col gap-1">
-            <img
-              src="https://images.pexels.com/photos/23369004/pexels-photo-23369004/free-photo-of-heron-flying-above-a-cherry-blossom.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-              alt="Shared media"
-            />
-            <p className="text-sm p-2 rounded-lg bg-[rgba(17,25,40,0.3)]">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi corporis cumque deleniti quaerat obcaecati
-              quasi assumenda a similique accusantium dolorem doloremque, qui totam eveniet quisquam reprehenderit ad
-              earum eaque libero!
-            </p>
-            <span className="text-xs">1 min ago</span>
-          </div>
-        </div>
+        )}
         <div ref={endRef}></div>
       </div>
 
       <div className="bottom flex p-5 items-center justify-between border-t border-t-[#dddddd35] gap-3 mt-auto">
-        <div className="icons flex gap-5">
-          <img
-            className="w-5 h-5 sm:w-[20px] sm:h-[20px] cursor-pointer"
-            src="./img.png"
-            alt="Upload"
+        <div className="flex gap-5 icons">
+          <label htmlFor="file">
+            <img
+              className="w-5 h-5 sm:w-[20px] sm:h-[20px] cursor-pointer"
+              src="./img.png"
+              alt="Upload"
+            />
+          </label>
+          <input
+            id="file"
+            type="file"
+            style={{ display: "none" }}
+            onChange={handleImg}
           />
           <img
             className="w-5 h-5 sm:w-[20px] sm:h-[20px] cursor-pointer"
@@ -193,7 +221,7 @@ export default function Chat() {
           onChange={(e) => setText(e.target.value)}
         />
 
-        <div className="emoji relative">
+        <div className="relative emoji">
           <img
             className="w-5 h-5 cursor-pointer"
             src="./emoji.png"
@@ -211,7 +239,10 @@ export default function Chat() {
           )}
         </div>
 
-        <button className="bg-[#5183fe] text-white text-sm px-2 py-1 sm:px-3 sm:py-1 rounded-[5px] cursor-pointer border-none">
+        <button
+          onClick={handleSend}
+          className="bg-[#5183fe] text-white text-sm px-2 py-1 sm:px-3 sm:py-1 rounded-[5px] cursor-pointer border-none"
+        >
           Send
         </button>
       </div>
--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { doc, onSnapshot, getDoc } from "firebase/firestore";
-import { useUserStore } from "../../../lib/userStore";
 import AddUser from "./addUser/addUser";
 import { db } from "../../../lib/firebase";
+import { doc, onSnapshot, getDoc, updateDoc } from "firebase/firestore";
+import { useUserStore } from "../../../lib/userStore";
+import { useChatStore } from "../../../lib/chatStore";
 
 export default function Chatlist() {
   const [chats, setChats] = useState([]);
   const [addMode, setAddMode] = useState(false);
 
   const { currentUser } = useUserStore();
+  const { chatId, changeChat } = useChatStore();
 
   useEffect(() => {
     const unSub = onSnapshot(doc(db, "userchats", currentUser.id), async (res) => {
@@ -33,6 +35,26 @@ export default function Chatlist() {
     };
   }, [currentUser.id]);
 
+  const handleSelect = async (chat) => {
+    const userChats = chats.map((item) => {
+      const { user, ...rest } = item;
+      return rest;
+    });
+
+    const chatIndex = userChats.findIndex((item) => item.chatId === chat.chatId);
+
+    userChats[chatIndex].isSeen = true;
+
+    const userChatsRef = doc(db, "userchats", currentUser.id);
+
+    try {
+      await updateDoc(userChatsRef, { chats: userChats });
+      changeChat(chat.chatId, chat.user);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="flex-1 overflow-auto">
       <div className="flex flex-col p-3">
@@ -50,24 +72,24 @@ export default function Chatlist() {
               placeholder="Search"
             />
           </div>
-          <div
-            className="bg-[rgba(17,25,40,0.5)] p-2 cursor-pointer rounded-lg"
-            onClick={() => setAddMode((prev) => !prev)}
-          >
+          <div className="bg-[rgba(17,25,40,0.5)] p-2 cursor-pointer rounded-lg">
             <img
               className="w-5 h-5"
               src={addMode ? "./minus.png" : "./plus.png"}
               alt="add"
+              onClick={() => setAddMode((prev) => !prev)}
             />
           </div>
         </div>
 
-        {/* Chat */}
+        {/* Chat list*/}
         <div>
           {chats.map((chat) => (
             <div
-              key={chat.chatId}
               className="flex items-center gap-3 p-3 cursor-pointer border-b border-b-[#dddddd35]"
+              key={chat.chatId}
+              onClick={() => handleSelect(chat)}
+              style={{ backgroundColor: chat?.isSeen ? "transparent" : "#5183fe" }}
             >
               <img
                 className="object-cover w-12 h-12 rounded-full"
